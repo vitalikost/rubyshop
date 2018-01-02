5f2c6dd5d2d5dd5c7e2b01ea75590d02e03b3e16
vitalikost <vitalikostrovskij@yandex.ru>
2018-01-02 16:27:45 +0200

    Добавлен майлер

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,14 @@
+class UserMailer < ApplicationMailer
+  def email_user(order)
+    @order = order
+
+    mail(to: @order.email, subject: 'Заказ номер №'+@order.id.to_s)
+  end
+
+  def email_admin(order)
+    @email = 'admin_shop@example.com'
+    @order = order
+    mail(to: @email, subject: 'Поступил новый заказ')
+  end
+
+end
